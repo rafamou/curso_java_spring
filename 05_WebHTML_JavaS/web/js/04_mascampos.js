d6f10e8539763c75c8128a5e7099e30ec787d740
rafamou <ragab.fayez2017@gmail.com>
2019-05-16 16:34:58 +0200

    HTML Y JS el dia 16 por la manana

---
+++ 05_WebHTML_JavaS/web/js/04_mascampos.js
@@ -0,0 +1,16 @@
+/* 
+GPL 3.0
+ */
+console.log("hola por consola");
+console.log("mensaje de error");
+
+document.write("<h2> Subtitle de la Webff </h2>")
+
+
+function mostrarMensaje(mensaje){
+    alert("te dicen que " + mensaje);
+    
+    
+    
+}
+
